75c0f4365866a1f2514c90714ea3c5e3d67954d5
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2023-12-12 18:06:33 +0200

    remove splash text theme

--- lib/view/screen/splash_screen.dart
+++ lib/view/screen/splash_screen.dart
@@ -8,9 +8,11 @@ import '../../core/helper/responsive.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   AnimationController? animationController;
@@ -27,9 +29,33 @@ class _SplashScreenState extends State<SplashScreen>
     animationController?.repeat(reverse: true);
     getToNewScreen();
   }
+
+  void getToNewScreen() {
+    Future.delayed(
+      const Duration(
+        seconds: 3,
+      ),
+      () {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) => FirebaseAuth.instance.currentUser == null
+                    ? const LoginScreen()
+                    : const NavbarScreen()));
+        log("screen height: ${ScreenSize.height} px");
+        log("screen width: ${ScreenSize.width} px");
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    animationController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
     ScreenSize.init(context);
     return Scaffold(
       backgroundColor: Colors.white,
@@ -54,18 +80,31 @@ class _SplashScreenState extends State<SplashScreen>
                             height: 110,
                           ))),
                   Text("Welcome to\n Doctor AI ",
-                      textAlign: TextAlign.center, style: textTheme.bodyMedium),
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.roboto(
+                          textStyle: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                      ))),
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 10),
                     child: Text(
                         "start chatting with your medical assassistant Now.You can ask me onything",
                         textAlign: TextAlign.center,
-                        style: textTheme.headlineMedium),
+                        style: GoogleFonts.roboto(
+                            textStyle: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.grey))),
                   ),
                   const Spacer(),
                   Text(
                     "V 0.0.01",
-                    style: textTheme.bodySmall,
+                    style: GoogleFonts.roboto(
+                        textStyle: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.green)),
                   )
                 ],
               ),
@@ -74,26 +113,5 @@ class _SplashScreenState extends State<SplashScreen>
         ],
       ),
     );
-  }
-
-  void getToNewScreen() {
-    Future.delayed(
-      const Duration(
-        seconds: 3,
-      ),
-      () {
-        Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-                builder: (context) => FirebaseAuth.instance.currentUser == null
-                    ? const LoginScreen()
-                    : const NavbarScreen()));
-      },
-    );
-  }
-  @override
-  void dispose() {
-    animationController?.dispose();
-    super.dispose();
   }
 }
