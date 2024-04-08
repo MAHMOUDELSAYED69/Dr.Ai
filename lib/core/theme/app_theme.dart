830eef780d5024232cf215cb7546b2bdc6700ae4
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-04-08 10:38:03 +0200

    create app theme class.

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,118 @@
+import 'package:dr_ai/core/constant/color.dart';
+import 'package:dr_ai/core/constant/fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class AppTheme {
+  //!! LIGHT THEME
+  static ThemeData get lightTheme {
+    return ThemeData(
+      iconTheme: const IconThemeData(color: ColorManager.grey),
+      switchTheme: SwitchThemeData(
+        trackOutlineColor: MaterialStateProperty.all(ColorManager.green),
+        thumbColor: MaterialStateProperty.all(ColorManager.white),
+        trackColor: MaterialStateProperty.all(ColorManager.green),
+        thumbIcon: MaterialStateProperty.all(const Icon(
+          Icons.light_mode,
+          color: ColorManager.white,
+        )),
+      ),
+      fontFamily: FontFamilyManager.poppins,
+      useMaterial3: true,
+      brightness: Brightness.light,
+      scaffoldBackgroundColor: ColorManager.white,
+      appBarTheme: AppBarTheme(
+        color: ColorManager.white,
+        iconTheme: const IconThemeData(color: ColorManager.black),
+        elevation: 0,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          color: ColorManager.black,
+          fontSize: 20.sp,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20.dm),
+            bottomRight: Radius.circular(20.dm),
+          ),
+        ),
+      ),
+      textTheme: TextTheme(
+        bodyLarge: TextStyle(
+          fontSize: 24.sp,
+          color: ColorManager.black,
+          fontWeight: FontWeight.w600,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 16.sp,
+          color: ColorManager.black,
+          fontWeight: FontWeight.w500,
+        ),
+        bodySmall: TextStyle(
+          fontSize: 14.sp,
+          color: ColorManager.grey,
+          fontWeight: FontWeight.w400,
+        ),
+        //* GREEN
+        displayLarge: TextStyle(
+          fontSize: 24.sp,
+          color: ColorManager.green,
+          fontWeight: FontWeight.w600,
+        ),
+        //! For button
+        displayMedium: TextStyle(
+          fontSize: 16.sp,
+          color: ColorManager.white,
+          fontWeight: FontWeight.w600,
+        ),
+        displaySmall: TextStyle(
+          fontSize: 14.sp,
+          color: ColorManager.grey,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        hintStyle: TextStyle(fontSize: 14.sp),
+        filled: true,
+        fillColor: ColorManager.white,
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20.dm),
+              bottomRight: Radius.circular(20.dm)),
+          borderSide: const BorderSide(
+            width: 2,
+            color: ColorManager.green,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20.dm),
+              bottomRight: Radius.circular(20.dm)),
+          borderSide: const BorderSide(
+            width: 3,
+            color: ColorManager.grey,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20.dm),
+              bottomRight: Radius.circular(20.dm)),
+          borderSide: const BorderSide(
+            width: 3,
+            color: ColorManager.error,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20.dm),
+              bottomRight: Radius.circular(20.dm)),
+          borderSide: const BorderSide(
+            width: 3,
+            color: ColorManager.error,
+          ),
+        ),
+        contentPadding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 20.w),
+      ),
+    );
+  }
+}
