33f966c8c8febdac40b156572713476ae632e720
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-04-28 03:01:25 +0300

    modifing google service in android folder.

--- lib/view/widget/social_login_card.dart
+++ lib/view/widget/social_login_card.dart
@@ -1,7 +1,6 @@
 import 'package:dr_ai/core/constant/image.dart';
 import 'package:dr_ai/core/helper/extention.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
