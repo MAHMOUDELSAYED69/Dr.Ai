c3254db769e68ebc5f841916a85fc2811fbdc5d0
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-06-26 14:52:24 +0300

    create setListOfMaps and setLastUpdatedTime in cachedata class.

--- lib/core/cache/cache.dart
+++ lib/core/cache/cache.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CacheData {
@@ -38,6 +39,29 @@ class CacheData {
   static Map<String, dynamic> getMapData({required String key}) {
     String jsonString = sharedpref.getString(key) ?? '{}';
     return jsonDecode(jsonString);
+  }
+
+  static Future<bool> setListOfMaps(
+      {required String key, required List<Map<String, dynamic>> value}) async {
+    String jsonString = jsonEncode(value);
+    return await sharedpref.setString(key, jsonString);
+  }
+
+  static List<Map<String, dynamic>> getListOfMaps({required String key}) {
+    String jsonString = sharedpref.getString(key) ?? '[]';
+    List<dynamic> jsonList = jsonDecode(jsonString);
+    return jsonList.map((e) => e as Map<String, dynamic>).toList();
+  }
+
+  static Future<void> setLastUpdatedTime(String key) async {
+    DateTime now = DateTime.now();
+    DateFormat formatter = DateFormat('yyyy-MM-dd hh:mm a');
+    String formattedDate = formatter.format(now);
+    await sharedpref.setString(key, formattedDate);
+  }
+
+  static String? getLastUpdatedTime(String key) {
+    return sharedpref.getString(key);
   }
 
   static dynamic getdata({required String key}) {
