8a41b11f964be694941c48ad48e2403cb09d3d31
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-06-26 14:55:21 +0300

    create FindHospitalWebService class and get hospital and place details.

--- lib/data/service/api/maps_place.dart
+++ lib/data/service/api/maps_place.dart
@@ -2,6 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:dr_ai/core/constant/api_url.dart';
 import 'dart:developer';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uuid/uuid.dart';
+
+import '../../model/find_hospital_place_info.dart';
 
 class PlacesWebservices {
   static Dio dio = Dio();
@@ -126,6 +129,109 @@ class PlacesWebservices {
       }
     } catch (err) {
       log(err.toString());
+    }
+  }
+}
+
+class FindHospitalWebService {
+  static final Dio dio = Dio();
+
+  static Future<List<FindHospitalsPlaceInfo>> getNearestHospital(
+      double latitude, double lngitude, double? radius) async {
+    final String sessionToken = const Uuid().v4();
+    List<FindHospitalsPlaceInfo> hospitals = [];
+    String? nextPageToken;
+
+    do {
+      log('call getNearestHospital');
+      try {
+        final response = await dio.get(
+          ApiUrlManager.nearestHospital,
+          queryParameters: {
+            'location': '$latitude,$lngitude',
+            'radius': radius?.toString() ?? '5000',
+            'type': 'hospital',
+            'key': ApiUrlManager.googleMapApiKey,
+            'sessiontoken': sessionToken,
+            if (nextPageToken != null) 'pagetoken': nextPageToken,
+          },
+        );
+
+        if (response.data == null || response.data['results'] == null) {
+          log('Response data is null or missing results key');
+          return hospitals;
+        }
+
+        final List<dynamic> results = response.data['results'];
+        for (var item in results) {
+          final placeId = item['place_id'];
+          final details = await _getPlaceDetails(placeId, latitude, lngitude);
+          if (details != null) {
+            hospitals.add(details);
+          }
+        }
+
+        nextPageToken = response.data['next_page_token'];
+        await Future.delayed(const Duration(seconds: 2));
+      } catch (err) {
+        log('Error: $err');
+        return hospitals;
+      }
+    } while (hospitals.length < 60 && nextPageToken != null);
+
+    return hospitals;
+  }
+
+  static Future<FindHospitalsPlaceInfo?> _getPlaceDetails(
+      String placeId, double originLat, double originLng) async {
+    log('call _getPlaceDetails');
+    try {
+      final response =
+          await dio.get(ApiUrlManager.placeLocation, queryParameters: {
+        'place_id': placeId,
+        'key': ApiUrlManager.googleMapApiKey,
+      });
+
+      final placeDetails = response.data['result'];
+      final distanceAndDuration = await _getDistanceAndDuration(
+        originLat,
+        originLng,
+        placeDetails['geometry']['location']['lat'],
+        placeDetails['geometry']['location']['lng'],
+      );
+
+      return FindHospitalsPlaceInfo.fromJson({
+        ...placeDetails,
+        'distance': distanceAndDuration['distance'],
+        'duration': distanceAndDuration['duration'],
+      });
+    } catch (err) {
+      log('Error fetching place details: $err');
+      return null;
+    }
+  }
+
+  static Future<Map<String, String>> _getDistanceAndDuration(double originLat,
+      double originLng, double destLat, double destLng) async {
+    log('call _getDistanceAndDuration');
+    try {
+      final response = await dio.get(
+        "https://maps.googleapis.com/maps/api/distancematrix/json",
+        queryParameters: {
+          'origins': '$originLat,$originLng',
+          'destinations': '$destLat,$destLng',
+          'key': ApiUrlManager.googleMapApiKey,
+        },
+      );
+
+      final element = response.data['rows'][0]['elements'][0];
+      return {
+        'distance': element['distance']['text'],
+        'duration': element['duration']['text'],
+      };
+    } catch (err) {
+      log('Error fetching distance matrix: $err');
+      return {};
     }
   }
 }
